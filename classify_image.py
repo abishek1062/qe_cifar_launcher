b9478687803733fd34200e25fbd96d6d3259c358
abishek1062 <abishek.s@outlook.com>
2020-10-26 16:57:27 +0530

    .cuda() removed - cpu inference enabled

--- classify_image.py
+++ classify_image.py
@@ -18,7 +18,7 @@ def recognizeImage(uploadfile,localfile):
         imagefile = localfile
 
 
-    gpu_available = torch.cuda.is_available()
+    # gpu_available = torch.cuda.is_available()
 
     image = get_image(imagefile)
 
@@ -27,14 +27,14 @@ def recognizeImage(uploadfile,localfile):
 
     model = get_model()
 
-    output_tensor = model(image.cuda())
+    output_tensor = model(image)
     output_tensor = Softmax(dim=1)(output_tensor)
     prob_pred_tensor, pred_tensor = torch.max(output_tensor, 1)
 
-    output = np.squeeze(output_tensor.detach().numpy()) if not gpu_available else np.squeeze(output_tensor.cpu().detach().numpy())
+    output = np.squeeze(output_tensor.detach().numpy()) 
 
-    prob_pred = np.squeeze(prob_pred_tensor.detach().numpy()) if not gpu_available else np.squeeze(prob_pred_tensor.cpu().detach().numpy())    
-    pred = np.squeeze(pred_tensor.detach().numpy()) if not gpu_available else np.squeeze(pred_tensor.cpu().detach().numpy())
+    prob_pred = np.squeeze(prob_pred_tensor.detach().numpy())
+    pred = np.squeeze(pred_tensor.detach().numpy())
 
     classes = ['airplane', 'automobile', 'bird', 'cat', 'deer','dog', 'frog', 'horse', 'ship', 'truck']
 
